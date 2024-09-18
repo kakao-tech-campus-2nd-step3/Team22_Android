7fc65e9aa11b2e46c5fe4d1590b29f30f0b623d8
akuby21 <akuby21@gmail.com>
2024-09-18 19:45:12 +0900

    chore : add packages for main
    add java/com/team22/soundary packages for ide sort

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
