2b9dbf0f1b8d2207dd54c21241c84bf48a492823
akuby21 <akuby21@gmail.com>
2024-09-19 03:27:43 +0900

    design: implement mainActivity layout
    implemented layout for mainActivity
    
    onclick event of each view will be handled after this week

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,11 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("de.hdodenhof:circleimageview:3.1.0") //Circle ImageView
 }
---
+++ app/src/main/java/com/team22/soundary/main/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.team22.soundary.main
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.team22.soundary.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
