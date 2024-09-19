121cad9a982f96929e13fa5e79fc7108ac6ad500
anyooin <anyooin123@naver.com>
2024-09-20 04:04:58 +0900

    design : 친구 선택 페이지 완성

--- app/src/main/java/com/team22/soundary/ShareFriendActivity.kt
+++ app/src/main/java/com/team22/soundary/ShareFriendActivity.kt
@@ -3,9 +3,9 @@ package com.team22.soundary;
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class ShareWhoActivity : AppCompatActivity() {
+class ShareFriendActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_share_who)
+        setContentView(R.layout.activity_share_friend)
     }
 }
