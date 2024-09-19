a593407c30b272692f3d84e8036d4b0c5d7c7cfe
anyooin <anyooin123@naver.com>
2024-09-20 02:48:29 +0900

    design : 공유페이지, 친구페이지
    만들다가 자꾸 오류나서 저장 겸 임시로 한번 올리고 수정할게요!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("kotlin-kapt")
 }
 
 android {
@@ -33,6 +34,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        dataBinding = true
+        buildConfig = true
+    }
 }
 
 dependencies {
@@ -42,6 +47,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("de.hdodenhof:circleimageview:3.1.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/team22/soundary/ShareWhoActivity.kt
@@ -0,0 +1,11 @@
+package com.team22.soundary;
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ShareWhoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_share_who)
+    }
+}
