14d50cf5baf6a77f70daa83220d7c19026118530
akuby21 <akuby21@gmail.com>
2024-09-20 15:57:22 +0900

    chore: move MainActivity to FriendSearchActivity
    change to appropirate name

---
+++ app/src/main/java/com/team22/soundary/FriendSearchActivity.kt
@@ -0,0 +1,14 @@
+package com.team22.soundary
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FriendSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friend_search)
+    }
+}
