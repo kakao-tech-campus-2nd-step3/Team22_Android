16a4afb4ba3692156c03ced8d27d6f5418fc8ba5
akuby21 <akuby21@gmail.com>
2024-09-20 18:34:33 +0900

    chore: unify dimen values and file names
    unify file name and dimen value to convention

--- app/src/main/java/com/team22/soundary/MainActivity.kt
+++ app/src/main/java/com/team22/soundary/MainActivity.kt
@@ -7,12 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /*setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
         setContentView(R.layout.widget_main)
     }
 }
