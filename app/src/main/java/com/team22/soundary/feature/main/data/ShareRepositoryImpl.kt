32a7d1bca8e952eef9f891ad48b28b0770542144
akuby21 <akuby21@gmail.com>
2024-09-27 20:59:15 +0900

    fix: add more mock datas

--- app/src/main/java/com/team22/soundary/feature/main/data/ShareRepositoryImpl.kt
+++ app/src/main/java/com/team22/soundary/feature/main/data/ShareRepositoryImpl.kt
@@ -14,20 +14,32 @@ class ShareRepositoryImpl : ShareRepository {
     private var mockDataList: MutableList<ShareEntity> = mutableListOf(
         ShareEntity(
             UserEntity("", "춘식이", "", "", "", Date(), Date()),
-            SongEntity("스토커", "10cm", "", "", "", Date()),
+            SongEntity("", "스토커", "", "", "10cm", Date()),
             "이거듣고 눈물흘림",
             false
         ),
         ShareEntity(
             UserEntity("", "춘식이", "", "", "", Date(), Date()),
-            SongEntity("Celebrity", "IU", "", "", "", Date()),
+            SongEntity("", "Celebrity", "", "", "IU", Date()),
             "좋은 노래네요",
             false
         ),
         ShareEntity(
             UserEntity("", "라이언", "", "", "", Date(), Date()),
-            SongEntity("Dynamite", "BTS", "", "", "", Date()),
-            "신나는 노래!",
+            SongEntity("", "Dynamite", "", "", "BTS", Date()),
+            "신나는 노래 추천!",
+            false
+        ),
+        ShareEntity(
+            UserEntity("", "춘식이", "", "", "", Date(), Date()),
+            SongEntity("", "Tokyo Flash", "", "", "Vaundy", Date()),
+            "노래입니다.",
+            false
+        ),
+        ShareEntity(
+            UserEntity("", "콘", "", "", "", Date(), Date()),
+            SongEntity("", "Supernova", "", "", "aespa", Date()),
+            "내 최애 노래야!",
             false
         ),
     )
@@ -41,27 +53,31 @@ class ShareRepositoryImpl : ShareRepository {
 }
 
 fun ShareEntity.toVO(): Share {
+    val onNull = "unknown"
     return Share(
-        this.song?.toVO(),
-        this.message ?: "",
-        this.sentUser?.toVO(),
+        this.song?.toVO() ?: Song(onNull,onNull,onNull,onNull),
+        this.message ?: onNull,
+        this.sentUser?.toVO() ?: User(onNull,onNull),
         this.isLike ?: false
     )
 }
 
 fun SongEntity.toVO(): Song {
+    val onNull = "unknown"
     return Song(
-        this.title ?: "",
-        this.artist ?: "",
-        this.preview ?: "",
-        this.coverImage ?: ""
+        this.title ?: onNull,
+        this.artist ?: onNull,
+        this.preview ?: onNull,
+        this.coverImage ?: onNull
 
     )
 }
 
 fun UserEntity.toVO(): User {
+    val onNull = "unknown"
     return User(
-        this.nickname ?: "",
-        this.image ?: ""
+        this.nickname ?: onNull,
+        this.image ?: onNull
     )
 }
+
