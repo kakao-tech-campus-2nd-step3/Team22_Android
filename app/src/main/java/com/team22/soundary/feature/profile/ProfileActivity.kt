d6a615dd099e014c4a7650f85855986119e6b0d9
유지예 <jiyeyu@192.168.nate.com>
2024-09-27 11:32:11 +0900

    feat: gallery requested in mypage

--- app/src/main/java/com/team22/soundary/feature/profile/ProfileActivity.kt
+++ app/src/main/java/com/team22/soundary/feature/profile/ProfileActivity.kt
@@ -1,4 +1,91 @@
 package com.team22.soundary.feature.main.profile
 
-class ProfileActivity {
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.team22.soundary.R
+
+class ProfileActivity : AppCompatActivity() {
+
+    private val PERMISSION_REQUEST_CODE = 100
+    private lateinit var profileImageView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mypage)
+
+        profileImageView = findViewById(R.id.profile_imageview)
+        profileImageView.setOnClickListener {
+            if (checkAndRequestPermissions()) {
+                openGallery()
+            }
+        }
+    }
+
+    // 권한 체크 및 요청
+    private fun checkAndRequestPermissions(): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val readPermission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_IMAGES)
+            } else {
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+
+            val cameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+
+            val listPermissionsNeeded = ArrayList<String>()
+            if (readPermission != PackageManager.PERMISSION_GRANTED) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    listPermissionsNeeded.add(Manifest.permission.READ_MEDIA_IMAGES)
+                } else {
+                    listPermissionsNeeded.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+            }
+            if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(Manifest.permission.CAMERA)
+            }
+            if (listPermissionsNeeded.isNotEmpty()) {
+                ActivityCompat.requestPermissions(this, listPermissionsNeeded.toTypedArray(), PERMISSION_REQUEST_CODE)
+                return false
+            }
+        }
+        return true
+    }
+
+    // 갤러리 접근하기
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, PERMISSION_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PERMISSION_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            val selectedImage: Uri? = data.data
+            profileImageView.setImageURI(selectedImage)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openGallery()
+                } else {
+                    //만약 권한 거부 시 어떻게 할 것인가 - 추후 작성
+                }
+            }
+        }
+    }
 }
