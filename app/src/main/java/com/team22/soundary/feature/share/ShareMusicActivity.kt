348f09544736e2cdf04d8655f103abb4d9624c85
anyooin <anyooin123@naver.com>
2024-09-27 16:51:49 +0900

    design : sort spinner

--- app/src/main/java/com/team22/soundary/feature/share/ShareMusicActivity.kt
+++ app/src/main/java/com/team22/soundary/feature/share/ShareMusicActivity.kt
@@ -1,6 +1,8 @@
 package com.team22.soundary.feature.share;
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import com.team22.soundary.R
 
@@ -8,5 +10,13 @@ class ShareMusicActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_share_music)
+
+        // spinner
+        val spinner = findViewById<Spinner>(R.id.share_sort_spinner)
+        spinner.adapter = ArrayAdapter(
+            this,
+            R.layout.share_spinner_item,
+            resources.getStringArray(R.array.share_sort_array)
+        )
     }
 }
