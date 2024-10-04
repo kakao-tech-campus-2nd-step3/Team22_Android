399b914d821efee2044c54b3f308d2eade7b8de4
유지예 <jiyeyu@yujiyeui-MacBookAir.local>
2024-10-04 18:06:01 +0900

    chore: widget_authority

--- app/src/main/java/com/team22/soundary/feature/profile/MyWidgetProvider.kt
+++ app/src/main/java/com/team22/soundary/feature/profile/MyWidgetProvider.kt
@@ -16,18 +16,13 @@ class MyWidgetProvider : AppWidgetProvider() {
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        // 각 위젯을 업데이트합니다.
         for (appWidgetId in appWidgetIds) {
             val views = RemoteViews(context.packageName, R.layout.widget_main)
-
-            // 닉네임 설정
             val nickname = "쿠키즈"
             views.setTextViewText(R.id.widget_textview, nickname.first().toString())
 
-            // 이미지 설정 추가
             views.setImageViewResource(R.id.widget_imageview, R.drawable.widget_image)
 
-            // 알림 여부에 따른 점 표시
             val hasNotification = true // 알림이 있다고 가정
             if (hasNotification) {
                 views.setViewVisibility(R.id.widget_notification_dot, View.VISIBLE)
@@ -35,7 +30,7 @@ class MyWidgetProvider : AppWidgetProvider() {
                 views.setViewVisibility(R.id.widget_notification_dot, View.GONE)
             }
 
-            // 위젯 업데이트
+            //위젯 업데이트
             appWidgetManager.updateAppWidget(appWidgetId, views)
         }
     }
