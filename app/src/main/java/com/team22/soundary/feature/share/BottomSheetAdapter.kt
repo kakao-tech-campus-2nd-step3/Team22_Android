6b5099f57a5d0237f451156cfb50ed8025c14f11
anyooin <100949208+anyooin@users.noreply.github.com>
2024-10-08 11:09:52 +0900

    [5 Week] feature : 바텀시트에서 선택한 정보 넘기기 (#39)
    * feat : show two type of friendItem
    
    * feat : selectedItem backgroundChange
    
    * design : set transparent bottomSheet background
    
    * feat : send data from bottomSheet
    
    * feat : save comment
    
    * feat : selectFriendAll button
    
    * feat : set sendButtonText
    
    * refactor : change activity to fragment
    
    * refactor : chage name Activity to Fragment
    
    * refactor : use companion object
    
    ---------
    
    Co-authored-by: Jiwon Ryu <39649594+akuby21@users.noreply.github.com>

--- app/src/main/java/com/team22/soundary/feature/share/BottomSheetAdapter.kt
+++ app/src/main/java/com/team22/soundary/feature/share/BottomSheetAdapter.kt
@@ -1,18 +1,20 @@
 package com.team22.soundary.feature.share
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.team22.soundary.R
 import com.team22.soundary.databinding.ShareFriendItemNoImageBinding
+import com.team22.soundary.databinding.ShareFriendItemWithImageBinding
 import com.team22.soundary.feature.share.data.FriendItemEntity
 
 class BottomSheetAdapter(
-    private var friendItemList: List<FriendItemEntity>,
     private val listener: FriendItemClickListener
-) : ListAdapter<FriendItemEntity, BottomSheetAdapter.ViewHolder>(FriendItemDiffCallback()) {
-    class ViewHolder(
+) : ListAdapter<FriendItemEntity, RecyclerView.ViewHolder>(FriendItemDiffCallback()) {
+    class ViewHolderNoImage(
         private val binding: ShareFriendItemNoImageBinding,
         private val listener: FriendItemClickListener
     ) : RecyclerView.ViewHolder(binding.root) {
@@ -27,25 +29,75 @@ class BottomSheetAdapter(
         fun bind(friendItem: FriendItemEntity) {
             binding.shareFriendImage.text = friendItem.name[0].toString()
             binding.shareFriendTextview.text = friendItem.name
+            binding.shareGrayBackground.visibility =
+                if (friendItem.isSelected) View.VISIBLE else View.INVISIBLE
             item = friendItem
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ShareFriendItemNoImageBinding.inflate(
-            LayoutInflater.from(parent.context),
-            parent,
-            false
-        )
-        return ViewHolder(binding, listener)
+    class ViewHolderWithImage(
+        private val binding: ShareFriendItemWithImageBinding,
+        private val listener: FriendItemClickListener
+    ) : RecyclerView.ViewHolder(binding.root) {
+        lateinit var item: FriendItemEntity
+
+        init {
+            binding.root.setOnClickListener {
+                listener.onClick(it, item)
+            }
+        }
+
+        fun bind(friendItem: FriendItemEntity) {
+            binding.shareFriendImage.setImageResource(R.drawable.stalker)
+            binding.shareFriendTextview.text = friendItem.name
+            binding.shareGrayBackground.visibility =
+                if (friendItem.isSelected) View.VISIBLE else View.INVISIBLE
+            item = friendItem
+        }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(friendItemList[position])
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        when (viewType) {
+            NO_IMAGE -> {
+                val binding = ShareFriendItemNoImageBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return ViewHolderNoImage(binding, listener)
+            }
+
+            WITH_IMAGE -> {
+                val binding = ShareFriendItemWithImageBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return ViewHolderWithImage(binding, listener)
+            }
+
+            else -> throw IllegalArgumentException()
+        }
     }
 
-    override fun getItemCount(): Int {
-        return friendItemList.size
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is ViewHolderNoImage -> holder.bind(getItem(position))
+            is ViewHolderWithImage -> holder.bind(getItem(position))
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (getItem(position).image == null) {
+            NO_IMAGE
+        } else {
+            WITH_IMAGE
+        }
+    }
+
+    companion object {
+        const val NO_IMAGE = 0
+        const val WITH_IMAGE = 1
     }
 }
 
