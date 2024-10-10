5907ff1bd35b59fdf5dee62c6fd58212518399bd
anyooin <anyooin123@naver.com>
2024-10-11 05:10:35 +0900

    feat : select category event

--- app/src/main/java/com/team22/soundary/feature/share/ShareBottomSheet.kt
+++ app/src/main/java/com/team22/soundary/feature/share/ShareBottomSheet.kt
@@ -5,17 +5,15 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.RadioButton
-import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.team22.soundary.R
 import com.team22.soundary.databinding.BottomSheetBinding
+import com.team22.soundary.feature.share.data.Category
 import com.team22.soundary.feature.share.data.FriendItemEntity
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class ShareBottomSheet : BottomSheetDialogFragment(R.layout.bottom_sheet) {
@@ -46,8 +44,8 @@ class ShareBottomSheet : BottomSheetDialogFragment(R.layout.bottom_sheet) {
     private fun setSendButton() {  // 메인버전이랑 공유버전이랑 나눠야
         binding.bottomSheetSendButton.text = viewModel.getButtonText()
         binding.bottomSheetSendButton.setOnClickListener {
-            viewModel.updateSelectItemList()
             viewModel.setComment(binding.shareCommentEdittext.text.toString())
+            viewModel.getFilteredFriendList(null)
             dismiss()
         }
     }
@@ -59,7 +57,7 @@ class ShareBottomSheet : BottomSheetDialogFragment(R.layout.bottom_sheet) {
     private fun setRecyclerView(view: View) {
         adapter = BottomSheetAdapter(object : FriendItemClickListener {
             override fun onClick(v: View, selectItem: FriendItemEntity) {
-                viewModel.setItemVisibility(selectItem)
+                viewModel.setItemSelected(selectItem)
                 binding.shareSelectAllButton.isChecked = viewModel.isSelectedAll()
                 binding.bottomSheetSendButton.text = viewModel.getButtonText()
             }
@@ -68,7 +66,7 @@ class ShareBottomSheet : BottomSheetDialogFragment(R.layout.bottom_sheet) {
         binding.selectFriendRecyclerview.layoutManager = GridLayoutManager(view.context, 4)
 
         lifecycleScope.launch {
-            viewModel.friendList.collect {
+            viewModel.filteredFriendList.collect {
                 adapter.submitList(it)
             }
         }
@@ -78,26 +76,33 @@ class ShareBottomSheet : BottomSheetDialogFragment(R.layout.bottom_sheet) {
         binding.shareSelectAllButton.isChecked = viewModel.isSelectedAll()
         binding.shareSelectAllButton.setOnClickListener {
             if (binding.shareSelectAllButton.isChecked) {
-                viewModel.setItemVisibilityAll(true)
+                viewModel.setItemSelectedAll(true)
             } else {
-                viewModel.setItemVisibilityAll(false)
+                viewModel.setItemSelectedAll(false)
             }
             binding.bottomSheetSendButton.text = viewModel.getButtonText()
         }
     }
-    
+
     private fun setCategoryRadioButton() {
         binding.categoryRadioGroup.setOnCheckedChangeListener { group, checkedId ->
-            if(checkedId != -1) {
+            if (checkedId != -1) {
                 val selectedRadioButton = group.findViewById<RadioButton>(checkedId)
                 selectedRadioButton.setOnClickListener {
                     if (lastCheckedRadioButtonId == checkedId) { // 이미 선택된 RadioButton을 다시 클릭한 경우
                         binding.categoryRadioGroup.clearCheck()
                         lastCheckedRadioButtonId = null
-                        Log.d("uin", "전체선택해제")
+                        viewModel.getFilteredFriendList(null)
                     } else {
                         lastCheckedRadioButtonId = checkedId
-                        Log.d("uin", "" + checkedId + "선택")
+                        when (checkedId) {
+                            R.id.category_rnb -> viewModel.getFilteredFriendList(Category.RnB)
+                            R.id.category_hiphop -> viewModel.getFilteredFriendList(Category.hiphop)
+                            R.id.category_pop -> viewModel.getFilteredFriendList(Category.pop)
+                            R.id.category_rock -> viewModel.getFilteredFriendList(Category.rock)
+                            R.id.category_jpop -> viewModel.getFilteredFriendList(Category.jpop)
+                            R.id.category_dance -> viewModel.getFilteredFriendList(Category.dance)
+                        }
                     }
                 }
             }
