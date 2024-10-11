30b72f58fbcdc928e326ad3369d0dc9f9fb5a2cb
유지예 <jiyeyu@yujiyeui-MacBookAir.local>
2024-10-11 16:53:05 +0900

    feat: fragment viewmodel

--- app/src/main/java/com/team22/soundary/feature/profile/fragment/ProfileEditedFragment.kt
+++ app/src/main/java/com/team22/soundary/feature/profile/fragment/ProfileEditedFragment.kt
@@ -1,106 +1,38 @@
 package com.team22.soundary.feature.profile.fragment
 
-import android.Manifest
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.team22.soundary.databinding.FragmentMypageEditBinding
+import com.team22.soundary.feature.profile.domain.ProfileViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ProfileEditedFragment : Fragment() {
     private var _binding: FragmentMypageEditBinding? = null
     private val binding get() = _binding!!
-    private val PERMISSION_REQUEST_CODE = 100
+
+    private val profileViewModel: ProfileViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentMypageEditBinding.inflate(inflater,container,false)
+        _binding = FragmentMypageEditBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.profileImageview.setOnClickListener {
-            if (checkAndRequestPermissions()) {
-                openGallery()
-            }
-        }
+        profileViewModel.loadProfileData() // ViewModel에서 데이터 받기
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
     }
-
-    private fun checkAndRequestPermissions(): Boolean {
-        val permissionsNeeded = mutableListOf<String>()
-
-        // 갤러리 접근
-        val readPermission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_MEDIA_IMAGES)
-        } else {
-            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
-        }
-
-        if (readPermission != PackageManager.PERMISSION_GRANTED) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                permissionsNeeded.add(Manifest.permission.READ_MEDIA_IMAGES)
-            } else {
-                permissionsNeeded.add(Manifest.permission.READ_EXTERNAL_STORAGE)
-            }
-        }
-
-        // 권한 요청 필요 시 처리
-        return if (permissionsNeeded.isNotEmpty()) {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                permissionsNeeded.toTypedArray(),
-                PERMISSION_REQUEST_CODE
-            )
-            false
-        } else {
-            true
-        }
-    }
-
-    // 갤러리 열기
-    private fun openGallery() {
-        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        startActivityForResult(intent, PERMISSION_REQUEST_CODE)
-    }
-
-
-    //TODO : Fragment 변경에 맞게 아래 메서드 수정 필요해 보입니다.
-
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//        if (requestCode == PERMISSION_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
-//            val selectedImage: Uri? = data.data
-//            binding.profileImageView.setImageURI(selectedImage)  // 선택된 이미지 표시
-//        }
-//    }
-//
-//    // 권한 요청 결과 처리
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<String>,
-//        grantResults: IntArray
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        if (requestCode == PERMISSION_REQUEST_CODE) {
-//            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                openGallery()
-//            }
-//        }
-//    }
 }
--- app/src/main/java/com/team22/soundary/feature/profile/fragment/ProfileFragment.kt
+++ app/src/main/java/com/team22/soundary/feature/profile/fragment/ProfileFragment.kt
@@ -1,31 +1,21 @@
 package com.team22.soundary.feature.profile.fragment
 
-import android.Manifest
-import android.app.Activity
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.team22.soundary.databinding.FragmentMypageBinding
+import com.team22.soundary.feature.profile.domain.ProfileViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ProfileFragment : Fragment() {
     private var _binding: FragmentMypageBinding? = null
     private val binding get() = _binding!!
 
-    //registerForActivityResult 쓰기
-    private val galleryLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == Activity.RESULT_OK && result.data != null) {
-            val selectedImage: Uri? = result.data?.data
-            binding.profileImageview.setImageURI(selectedImage)
-        }
-    }
+    private val profileViewModel: ProfileViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,56 +26,13 @@ class ProfileFragment : Fragment() {
         return binding.root
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initProfileImageView()
+        profileViewModel.loadProfileData() // ViewModel에서 데이터 받기
     }
 
-    // 초기화 메소드 분리 1
-    private fun initProfileImageView() {
-        binding.profileImageview.setOnClickListener {
-            val permissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                arrayOf(Manifest.permission.READ_MEDIA_IMAGES)
-            } else {
-                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-            }
-
-            //TODO : Fragment에는 존재하지 않는 메서드. 수정 부탁드립니다
-//            if (checkAndRequestPermissions(permissions, PERMISSION_REQUEST_CODE)) {
-//                openGallery()
-//            }
-        }
-    }
-
-    // 갤러리 접근하기
-    private fun openGallery() {
-        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        galleryLauncher.launch(intent) //registerForActivityResult 사용하기
-    }
-
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            PERMISSION_REQUEST_CODE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    openGallery()
-                } else {
-                    //만약 권한 거부 시 어떻게 할 것인가 - 추후 작성
-                }
-            }
-        }
-    }
-    companion object{
-        private val PERMISSION_REQUEST_CODE = 100
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
