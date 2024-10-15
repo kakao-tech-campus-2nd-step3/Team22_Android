26675d344dec674f8a0fe6320d9d9cbbe218e340
Jiwon Ryu <39649594+akuby21@users.noreply.github.com>
2024-10-15 11:23:58 +0900

    [6 Week] Refactor: 메인 화면 리팩터링 (#47)
    * refactor: change FragmentManager
    
    * refactor: apply repeatOnLifeCycle for optimization
    
    * refactor: change method on collecting uiState
    
    collect -> collectLatest

--- app/src/main/java/com/team22/soundary/feature/main/MainFragment.kt
+++ app/src/main/java/com/team22/soundary/feature/main/MainFragment.kt
@@ -12,7 +12,9 @@ import android.widget.ArrayAdapter
 import androidx.activity.viewModels
 import androidx.core.view.isGone
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
@@ -24,9 +26,11 @@ import com.google.android.material.snackbar.Snackbar
 import com.team22.soundary.R
 import com.team22.soundary.databinding.ActivityMainBinding
 import com.team22.soundary.databinding.FragmentMainBinding
+import com.team22.soundary.feature.profile.ImageUtil
 import com.team22.soundary.feature.share.ShareBottomSheet
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import java.io.IOException
 
@@ -72,11 +76,10 @@ class MainFragment : Fragment() {
     }
 
     private fun setShareButton(){
-        //TODO : MainActivity로 BottomSheet 생성 위임하기
-//        binding.shareImageButton.setOnClickListener {
-//            val modal = ShareBottomSheet()
-//            modal.show(supportFragmentManager, ShareBottomSheet.TAG)
-//        }
+        binding.shareImageButton.setOnClickListener {
+            val modal = ShareBottomSheet()
+            modal.show(parentFragmentManager, ShareBottomSheet.TAG)
+        }
     }
 
     private fun setSongClickListener() {
@@ -182,12 +185,14 @@ class MainFragment : Fragment() {
 
     private fun updateMusicState() {
         lifecycleScope.launch {
-            while (true) {
-                if (player.isPlaying) {
-                    updateProgressBar()
-                    updateText()
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                while (true) {
+                    if (player.isPlaying) {
+                        updateProgressBar()
+                        updateText()
+                    }
+                    delay(PROGRESS_UPDATE_DELAY.toLong())
                 }
-                delay(PROGRESS_UPDATE_DELAY.toLong())
             }
         }
     }
@@ -244,24 +249,27 @@ class MainFragment : Fragment() {
 
     private fun observeUiState() {
         lifecycleScope.launch {
-            viewModel.uiState.collect { uiState ->
-                binding.friendNameTextView.text = uiState.friendName
-                binding.musicNameTextView.text = uiState.musicName
-                binding.singerTextView.text = uiState.singer
-                binding.messageTextView.text = uiState.message
-                binding.nextImageView.isGone = uiState.isLastSong
-                binding.prevImageView.isGone = uiState.isFirstSong
-                binding.likeButton.setImageResource(uiState.likeBackground)
-                uiState.friendImage?.let {
-                    Glide.with(requireContext())
-                        .load(it)
-                        .into(binding.friendPicImageView)
-                }
-                uiState.songImage?.let {
-                    Glide.with(requireContext())
-                        .load(it)
-                        .apply(RequestOptions.bitmapTransform(RoundedCorners(20)))
-                        .into(binding.currentImageView)
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collectLatest { uiState ->
+                    binding.friendNameTextView.text = uiState.friendName
+                    binding.musicNameTextView.text = uiState.musicName
+                    binding.singerTextView.text = uiState.singer
+                    binding.messageTextView.text = uiState.message
+                    binding.nextImageView.isGone = uiState.isLastSong
+                    binding.prevImageView.isGone = uiState.isFirstSong
+                    binding.likeButton.setImageResource(uiState.likeBackground)
+                    uiState.friendImage?.let {
+                        Glide.with(requireContext())
+                            .load(it)
+                            .circleCrop()
+                            .into(binding.friendPicImageView)
+                    }
+                    uiState.songImage?.let {
+                        Glide.with(requireContext())
+                            .load(it)
+                            .apply(RequestOptions.bitmapTransform(RoundedCorners(20)))
+                            .into(binding.currentImageView)
+                    }
                 }
             }
         }
